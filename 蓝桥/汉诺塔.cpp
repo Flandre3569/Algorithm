5c1f6f44f920f50335a1c5807ed089f007aa2999
Flandre3569 <1332726596@qq.com>
2022-03-29 22:09:14 +0800

    汉诺塔

---
+++ 蓝桥/汉诺塔.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+stack<int> s[3];
+void move(int x, int y)
+{
+    int temp = s[x].top();
+    s[x].pop();
+    s[y].push(temp);
+    cout << x << "-->" << y << endl;
+}
+
+void hanoi(int A, int B, int C, int n)
+{
+    if(n == 1) {
+        move(A, C);
+        return;
+    }
+    hanoi(A, C, B, n-1);
+    move(A, C);
+    hanoi(B, A, C, n-1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = n; i > 0; i--) {
+        s[0].push(i);
+    }
+
+    hanoi(0, 1, 2, n);
+
+    while(!s[2].empty()) {
+       cout << s[2].top() << " ";
+       s[2].pop();
+    }
+    return 0;
+}
