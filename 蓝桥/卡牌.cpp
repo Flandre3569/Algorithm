3b91f917df6bafdf5c7c0349d3b6ee5f15f674f9
Flandre3569 <1332726596@qq.com>
2022-04-09 22:48:41 +0800

    卡牌修改

--- 蓝桥/卡牌.cpp
+++ 蓝桥/卡牌.cpp
@@ -3,9 +3,32 @@ using namespace std;
 
 int main()
 {
-    for(int i = 0; i < 2021; i++) {
-
+    int sum[10];
+    for(int i = 0; i <= 9; i++)
+    {
+        // 每种卡牌都是2021张
+        sum[i] = 2021;
     }
+
+    int i;
+    for(i = 1;i <= 20210; i++)
+    {
+        int t = i,flag = 0;
+        while(t)
+        {
+            // 相应数值卡片减一
+            sum[t % 10]--;
+            if(sum[t % 10] == -1) {
+                flag = 1;
+                break;
+            }
+            // 进一位
+            t = t / 10;
+        }
+
+        if(flag) break;
+    }
+    cout << i-1;
 
     return 0;
 }
