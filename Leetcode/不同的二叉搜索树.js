391f6e0f95173b193892d993d629c5e2d2bbd110
Flandre3569 <1332726596@qq.com>
2022-04-23 15:45:04 +0800

    96

---
+++ Leetcode/不同的二叉搜索树.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+  let dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      dp[i] += dp[j - 1] * dp[i - j];
+    }
+  }
+
+  return dp[n];
+};
+
+const result = numTrees(3);
+console.log(result);
