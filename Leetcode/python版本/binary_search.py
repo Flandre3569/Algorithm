fdcb09e777501f698485a849ea3abe8b9dd38d60
Flandre3569 <1332726596@qq.com>
2024-11-14 20:53:28 +0800

    11.14 binary-search

--- Leetcode/python版本/binary_search.py
+++ Leetcode/python版本/binary_search.py
@@ -3,7 +3,18 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        target = 0
-        for item in nums:
-            if self == item:
-                target
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if(nums[mid] == target):
+                return mid
+            if(nums[mid] < target):
+                left = mid + 1
+            if(nums[mid] > target):
+                right = mid - 1
+
+        return -1
+
+s = Solution()
+x = s.search([1,2,3,4,5,6,7], 6)
+print(x)
